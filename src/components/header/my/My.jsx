452fdb6652ec5279efe653f1c54d97f8cf23a2e4
linH2407 <linhlinh24072000@gmail.com>
2022-03-16 17:32:30 +0700

    feat: add contact, fix: responsive

--- src/components/header/my/My.jsx
+++ src/components/header/my/My.jsx
@@ -5,6 +5,7 @@ const My = () => {
   return (
     <div className='my__container'>
       <div className='my__name'>
+        <h1>Hi, my name is</h1>
         <h2>Naruhito Takei.</h2>
         <h3>I build things for the web.</h3>
       </div>
